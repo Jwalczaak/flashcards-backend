b5b4e2f9d43feed87bd1099776b33d2a1f7884e3
Jwalczaak <j.walczaak@gmail.com>
2024-11-29 12:11:25 +0100

    Added base files

---
+++ src/models/flashcardModel.ts
@@ -0,0 +1,12 @@
+// import mongoose from "mongoose";
+// import { UserDocument } from "../interfaces/User";
+
+// const flashcardSchema = new mongoose.Schema<UserDocument>({
+//     name: {
+//         type:String,
+//         required: [true, 'Name is required'],
+//     },
+//     translation: {
+
+//     }
+// })
